175cfb54336444f040b05dfc9350ad958d232f87
sudofluff <sudofluff@icloud.com>
2019-09-02 23:51:03 -0700

    Removes faulty route

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,4 @@ Rails.application.routes.draw do
     mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
   end
   post "/graphql", to: "graphql#execute"
-  get "/graphql", to: "graphql#execute"
 end
